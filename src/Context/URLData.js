465c0a8e0abe558f8961a59ed929fa9123bb8438
Kareem <kareem.hisham@mimocodes.com>
2022-08-10 21:12:49 +0200

    first commit

---
+++ src/Context/URLData.js
@@ -0,0 +1,19 @@
+export const ExercisesOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "11eaaae57amsh20d583be2f33c9ep1e9fe1jsnfd8a0bcecb94",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const YoutubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "11eaaae57amsh20d583be2f33c9ep1e9fe1jsnfd8a0bcecb94",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
+export const FetchURL = "https://exercisedb.p.rapidapi.com/exercises";
+export const BodyPartsURL =
+  "https://exercisedb.p.rapidapi.com/exercises/bodyPartList";
